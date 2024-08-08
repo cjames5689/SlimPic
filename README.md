89228767bd85c532776ce6e2485a25b1cc5ef0b7
cjames5689 <cjames5689@gmail.com>
2024-08-07 21:34:21 -0400

    Logic cleanup
    Removed double import of the PIL module, added extra logic for reducing user input error, used normpath on user_input to convert the input to be usable on any system, cleaned up some variable naming conventions, moved the help menu to its own function for display. Overall cleanup of the functions that are present.
    
    Additionally changed the readme to match the new functionality.

--- README.md
+++ README.md
@@ -9,12 +9,13 @@
 -----------------------------------------------------------------------------------------------------------------------------
 
 OVERVIEW:
-The Photo Resizer is a simple program designed to resize all .jpg and .jpeg images in a specified directory to a resolution of 1600x1200 pixels. The resized images are saved in a subdirectory named resized.
+The Photo Resizer is a simple program designed to resize all .jpg images in a specified directory to a max resolution of 1600x1200 pixels. The resized images are saved in a subdirectory named 'resized'. If the photo's original resolution is lower than the 1600x1200 bounding box, the script using thumbnail() will leave the image unchanged. It will not upscale the image, so the original resolution is preserved without any resizing.
 -----------------------------------------------------------------------------------------------------------------------------
 
 USAGE:
-Navigate to the directory containing the photo_resizer.exe executable. This can be found in the dist folder.
-Run the executable main.exe.
+*Navigate to the directory containing the photo_resizer.exe executable. This can be found in the dist folder.
+
+*Run the executable 'main.exe'.
 -----------------------------------------------------------------------------------------------------------------------------
 
 Follow the prompts to enter the path of the directory containing the photos you want to resize. If you need help finding the directory path, type help for instructions.
@@ -23,15 +24,20 @@ The program will create a subdirectory named resized in the specified directory 
 
 INSTRUCTIONS:
 *Open Windows Explorer and navigate to the folder with the current work order's photos.
+
 *Click the address bar to highlight the path. (Refer to ADDRESS BAR EXAMPLE if necessary)
+
 *Copy the path and paste it into the program when prompted.
+
 *The program will resize all .jpg files in the specified directory and save the resized images in the resized subdirectory.
 -----------------------------------------------------------------------------------------------------------------------------
 
 NOTES:
-This program only processes .jpg and .jpeg files.
-Each photo will be resized to 1600x1200px.
-If the resized subdirectory already exists, the program will use it without creating a new one.
+*This program only processes .jpg and .jpeg files.
+
+*The script resizes images to fit within a maximum size of 1600x1200 pixels while preserving the original aspect ratio. This ensures that the images are scaled down proportionally without distortion, fitting within the bounding box while maintaining their natural dimensions.
+
+*If the resized subdirectory already exists, the program will use it without creating a new one.
 -----------------------------------------------------------------------------------------------------------------------------
 
 EXIT:
@@ -39,5 +45,6 @@ To exit the program at any time, type exit when prompted for a directory path.
 -----------------------------------------------------------------------------------------------------------------------------
 
 ERROR HANDLING:
-If an invalid directory path is entered, the program will prompt you to enter a valid path.
-If an error occurs while processing an image, the program will notify you and continue processing the remaining images.
+*If an invalid directory path is entered, the program will prompt you to enter a valid path.
+
+*If an error occurs while processing an image, the program will notify you and continue processing the remaining images.
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
-import PIL
-from PIL import Image
-
+import PIL.Image
 size = (1600, 1200)
 
 
 def main():
     while True:
         user_input = (input("(Type 'help' for instructions or 'exit' to exit the program.)\n"
-                            "Please enter the current working directory: ")).lower()
+                            "Please enter the current working directory: ")).lower().strip()
+        user_input = os.path.normpath(user_input)
         input_logic(user_input)
 
 
@@ -16,56 +15,58 @@ def make_folder(path):
     try:
         resized_path = os.path.join(path, "resized")
         os.makedirs(resized_path, exist_ok=True)
-        print(f"\n"
-              f"{resized_path} has been created.\n")
+        print(f"\n{resized_path} has been created.\n")
+        if os.path.isdir(resized_path):
+            return True
     except WindowsError as w:
         print(f"\nAccess denied. Contact administrator for directory access.\n{w}\n")
-        main()
     except Exception as e:
-        print(f"There was an error when creating the 'resized' folder.\nError code: {e}")
-        main()
+        print(f"\nThere was an error when creating the 'resized' folder.\nError code: {e}\n")
 
 
 def resize_photo(path):
     success_count = 0
     os.chdir(path)
-    jpg_jpeg_files = [
+    source_files = [
         picture for picture in os.listdir(path)
         if os.path.splitext(picture)[1].lower() in [".jpeg", ".jpg"]
     ]
-    total_files = len(jpg_jpeg_files)
-    if jpg_jpeg_files:
+    total_files = len(source_files)
+    if source_files:
         print("Resizing...")
         resized_dir = os.path.join(path, "resized")
-        for picture in jpg_jpeg_files:
+        for picture in source_files:
             picture_path = os.path.join(path, picture)
             resized_picture_path = os.path.join(resized_dir, picture)
+            resized_picture_name = os.path.basename(resized_picture_path)
             try:
                 if picture not in os.listdir(resized_dir):
-                    with Image.open(picture_path) as im:
+                    with PIL.Image.open(picture_path) as im:
                         im.thumbnail(size)
-                        im.save(os.path.join(path, "resized", picture))
+                        im.save(resized_picture_path)
                         success_count += 1
-                        print(f"{os.path.basename(resized_picture_path)} has been processed.")
+                        print(f"{resized_picture_name} has been processed.")
                 else:
-                    print("File already processed.")
+                    print(f"{resized_picture_name} already processed.")
             except PIL.UnidentifiedImageError as e:
                 print(f"\nUnknown file type. {picture} was not processed.\n{e}\n")
-        print(f"\n"
-              f"{success_count} of {total_files} files processed.\n")
+        print(f"\n{success_count} of {total_files} files processed.\n")
     else:
         print("\nNo files found to be processed.\n")
 
 
+def display_help():
+    print("\nTo get the current working directory:\n"
+          "1. Open Windows Explorer\n"
+          "2. Navigate to the folder with the current work order's photos\n"
+          "3. Click the address bar to highlight the path\n"
+          "4. Copy the path and paste it into this program\n"
+          "\nNote: This program will only resize .jpg/.jpeg files.\n")
+
+
 def input_logic(command):
     if command == "help":
-        print("\nTo get the current working directory:\n"
-              "1. Open Windows Explorer\n"
-              "2. Navigate to the folder with the current work order's photos\n"
-              "3. Click the address bar to highlight the path\n"
-              "4. Copy the path and paste it into this program\n"
-              "\n"
-              "Note: This program will only resize .jpg/.jpeg files.\n")
+        display_help()
     elif command == "exit":
         exit("User exited the program.")
     elif not command.strip():
@@ -73,8 +74,8 @@ def input_logic(command):
     elif not os.path.isdir(command):
         print("Invalid directory entry. Please enter a valid directory path.\n")
     else:
-        make_folder(command)
-        resize_photo(command)
+        if make_folder(command):
+            resize_photo(command)
 
 
 if __name__ == "__main__":
